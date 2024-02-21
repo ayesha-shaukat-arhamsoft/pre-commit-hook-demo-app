4ae3aab66e786d554b6bbcce707539e1abc3fe22
ayesha-shaukat-arhamsoft <78010723+ayesha-shaukat-arhamsoft@users.noreply.github.com>
2024-02-21 15:05:55 +0500

    t1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+const x  = 1;
 
 function App() {
-  const [count,    setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <>
